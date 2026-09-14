bf4b2c5d6d7ed31ce9f19bf203ecc5883b19e77f
mgulled204@gmail.com <mgulled204@gmail.com>
2026-09-14 18:57:52 +0000

    add seller to silver order_id

--- ecommerce_pipeline/transformations/silver/order_items.py
+++ ecommerce_pipeline/transformations/silver/order_items.py
@@ -21,6 +21,7 @@ def order_items_silver():
             F.trim(F.col("order_id")).alias("order_id"),
             F.col("order_item_id").alias("order_item_id"),
             F.trim(F.col("product_id")).alias("product_id"),
+            F.trim(F.col("seller_id")).alias("seller_id"),
             F.col("shipping_limit_date").alias("shipping_date"),
             F.col("price").alias("price"),
             F.col("freight_value").alias("freight_value"), 
